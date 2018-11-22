6eefcb20b07250d6cf6c4c41f04b86e0ac8ba4aa
Josh Bolland <jjm.bolland@gmail.com>
2018-11-22 08:39:47 +0000

    changed message to be text instead of string in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_174034) do
+ActiveRecord::Schema.define(version: 2018_11_22_083652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_174034) do
     t.bigint "user_id"
     t.bigint "slot_id"
     t.string "status", default: "pending"
-    t.string "message"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["slot_id"], name: "index_requests_on_slot_id"
