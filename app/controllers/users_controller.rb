8d92bed859624fabe543f161ae19a2f3f6274b45
Kaif Ahmed <hello@kaif.me>
2018-11-23 14:01:23 +0000

    temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @hosted_jams = @user.jams
     @hosted_slots = Slot.where(jam: @hosted_jams)
+    @confirmed_slots = 
     @pending_requests = Request.where(slot: @hosted_slots, status: "pending")
   end
 end
