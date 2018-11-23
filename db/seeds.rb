deca314f5541a077fed93c9abc6a09383127c1a2
Kaif Ahmed <hello@kaif.me>
2018-11-23 16:27:29 +0000

    Finished the seeds file. Finally.

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,18 +70,54 @@ def assign_instrument(instrument, start, stop)
 end
 
 # Begin Seeding
+# Nuke everything
 wipe_db
 
+# Seed instruments
 db_seed('instruments.yml') { |attributes| create_a_seed_instrument(attributes) }
 @instruments = Instrument.all
 
+# Seed users
 db_seed('users.yml') { |attributes| create_a_seed_user(attributes) }
 @users = User.all
-db_seed('jam01.yml') { |attributes| create_a_jam(attributes, User.where(first_name: "Die").first) }
-db_seed('jam02.yml') { |attributes| create_a_jam(attributes, User.where(first_name: "Lou").first) }
 
+# Assign instruments to users
 assign_instrument(@instruments.first, 0, 4)
 assign_instrument(@instruments.second, 3, 6)
 assign_instrument(@instruments.third, 7, 9)
 assign_instrument(@instruments.fourth, 2, 5)
 assign_instrument(@instruments.fifth, 8, 9)
+
+# Seed jams
+db_seed('jam01.yml') { |attributes| create_a_jam(attributes, User.where(first_name: "Winnie").first) }
+db_seed('jam02.yml') { |attributes| create_a_jam(attributes, User.where(first_name: "Lou").first) }
+db_seed('jam03.yml') { |attributes| create_a_jam(attributes, User.where(first_name: "Mabel").first) }
+db_seed('jam04.yml') { |attributes| create_a_jam(attributes, User.where(first_name: "Max").first) }
+
+# Assign slots to jams
+puts "Adding slots to jams..."
+Slot.create!(jam: Jam.first, instrument: Instrument.first)
+Slot.create!(jam: Jam.first, instrument: Instrument.second, user: @users[3])
+Slot.create!(jam: Jam.first, instrument: Instrument.third, user: @users[7])
+Slot.create!(jam: Jam.first, instrument: Instrument.fourth)
+
+Slot.create!(jam: Jam.second, instrument: Instrument.first)
+Slot.create!(jam: Jam.second, instrument: Instrument.second)
+Slot.create!(jam: Jam.second, instrument: Instrument.fourth, user: @users[2])
+
+Slot.create!(jam: Jam.third, instrument: Instrument.first, user: @users[0])
+Slot.create!(jam: Jam.third, instrument: Instrument.second)
+Slot.create!(jam: Jam.third, instrument: Instrument.third, user: @users[8])
+Slot.create!(jam: Jam.third, instrument: Instrument.fifth)
+
+Slot.create!(jam: Jam.fourth, instrument: Instrument.first)
+Slot.create!(jam: Jam.fourth, instrument: Instrument.first)
+Slot.create!(jam: Jam.fourth, instrument: Instrument.second, user: @users[4])
+Slot.create!(jam: Jam.fourth, instrument: Instrument.third, user: @users[8])
+puts "DONE!"
+
+puts "Making requests on empty slots..."
+Slot.where(user_id: nil).each do |slot|
+  Request.create!(slot: slot, user: User.all.sample, message: "Please can I play #{slot.instrument.name}!")
+end
+puts "DONE!"
